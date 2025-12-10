e06c1261c65858aed79efbfe12d41c332fb064cd
Daniel Ardekani <ardekanicapital@gmail.com>
2025-12-10 17:01:14 -0500

    Fix language parameter in HotelBeds API request

--- src/services/affiliates/hotelbeds.js
+++ src/services/affiliates/hotelbeds.js
@@ -86,7 +86,7 @@ export async function fetchDestinations() {
 
   try {
     const response = await fetch(
-      `${CONTENT_API_BASE}/locations/destinations?fields=all&language=en`,
+      `${CONTENT_API_BASE}/locations/destinations?fields=all&language=eng`,
       {
         method: 'GET',
         headers: getHeaders()
